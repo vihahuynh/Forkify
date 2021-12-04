4e5daa1bb2c3c65c9f2072f128e04f63e2270fdf
vihahuynh <huynhviha1703@gmail.com>
2021-12-04 20:21:19 +0700

    fix error Fraction

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -121,7 +121,9 @@ class RecipeView extends View {
       <svg class="recipe__icon">
         <use href="${icons}#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${quantity ? quantity.toFixed(2) : ''}</div>
+      <div class="recipe__quantity">${
+        ing.quantity ? ing.quantity.toFixed(2) : ''
+      }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
         ${ing.description}
